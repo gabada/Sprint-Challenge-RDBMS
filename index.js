3b9c36acceccfccca5413552496ea7e36a17bae9
Gill Abada <gill@callifi.com>
2019-03-29 11:46:55 -0400

    add projects router in index.js.

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const helmet = require('helmet');
 const server = express();
 const port = 5000;
 
+const projectsRouter = require('./projects/projects-router.js');
+
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
+
+server.use('/api/projects', projectsRouter);
 
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
